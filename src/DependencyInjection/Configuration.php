f8dff152eb6ff6519d1d10e4b258231cfc042918
MrNicodemuz <nicohome@gmail.com>
2014-03-29 17:26:15 +0800

    Add support for memcache API.
    Add support for older memcache API implementation.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -70,7 +70,7 @@ class Configuration implements ConfigurationInterface
             ->prototype('array')
                 ->children()
                     ->enumNode('type')
-                        ->values(array('redis', 'file', 'memcached', 'apc'))
+                        ->values(array('redis', 'file', 'memcached', 'memcache', 'apc'))
                     ->end()
                     ->scalarNode('id')
                         ->defaultNull()
